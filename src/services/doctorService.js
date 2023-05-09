17d25357fc6b7cc9a0ad0020e9bbd656f74c0177
manh <duymanh311999@gmail.com>
2023-05-09 22:24:10 +0700

    Bai 77

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -161,15 +161,9 @@ let bulkCreateSchedule = (data) => {
                         raw: true
                     }             
                 );
-                if(existing && existing.length > 0){
-                    existing = existing.map(item=> {
-                        item.date = new Date( item.date).getTime(); 
-                        return item
-                    })
-                }
 
                 let toCreate = _.differenceWith(schedule, existing, (a, b)=>{
-                    return a.timeType === b.timeType && a.date === b.date;
+                    return a.timeType === b.timeType && +a.date === +b.date;
                 })
                 if(toCreate && toCreate.length > 0){
                     await db.Schedule.bulkCreate(toCreate);
@@ -199,7 +193,13 @@ let getScheduleByDate = (doctorId, date) => {
                     where: {
                         doctorId: doctorId,
                         date: date
-                    }
+                    },
+                    include: [                  
+                        {model: db.Allcode, as: 'timeTypeData', attributes: ['valueEn', 'valueVi']},
+
+                    ],
+                     raw: false,
+                     nest: true   
                 })
 
                 if(!dataSchedule) dataSchedule =[];
