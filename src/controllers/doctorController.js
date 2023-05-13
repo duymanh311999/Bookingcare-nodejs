b379d9cffca617f37c63b41296b98d430012acbc
manh <duymanh311999@gmail.com>
2023-05-13 15:45:49 +0700

    Bai 84

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -107,6 +107,24 @@ let getExtraInforDoctorById = async (req, res) => {
     }
 }
 
+
+let getProfileDoctorById = async (req, res) => {
+    try{
+        let infor = await doctorService.getProfileDoctorById(req.query.doctorId);
+        return res.status(200).json(
+            infor
+        )
+
+    }catch(e){
+        console.log(e);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server'
+        })
+    }
+}
+
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
@@ -115,4 +133,5 @@ module.exports = {
     bulkCreateSchedule: bulkCreateSchedule,
     getScheduleByDate: getScheduleByDate,
     getExtraInforDoctorById: getExtraInforDoctorById,
+    getProfileDoctorById: getProfileDoctorById,
 }
