d6af81dc119c293e8ff32e988155ab0b0df333ed
manh <duymanh311999@gmail.com>
2023-05-15 16:39:13 +0700

    Bai 88

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 import nodemailer from "nodemailer";
 
 let sendSimpleEmail = async (dataSend) => {
@@ -18,24 +19,50 @@ let sendSimpleEmail = async (dataSend) => {
     from: '"Duy Manh" <duymanh311999@gmail.com>', // sender address
     to: dataSend.receiverEmail, // list of receivers
     subject: "Thông tin đặt lịch khám bệnh", // Subject line
-    html: `
-       <h3>Xin chào ${dataSend.patientName}</h3>
-       <p>Bận nhận được Email này vì đã đặt lịch khám bệnh tại Booking Care thành công</p> 
-       <p>Thông tin đặt lịch khám bệnh:</p>
-        <div><b>Thời gian: ${dataSend.time}</b></div>
-        <div><b>Bác sĩ: ${dataSend.doctorName}</b></div>
-
-        <p>Nếu các thông tin trên là đúng, vui lòng nhấn vào đường link bên dưới
-        để xác nhận và hoàn tất thủ tục đặt lịch khám bệnh.
-        </p>
-        <div>
-            <a href=${dataSend.redirectLink} target="_blank">Click here</a>   
-            <div>Xin chân thành cảm ơn</div>     
-        </div>
-    `, // html body
+    html: getBodyHTMLEmail(dataSend), // html body
   });
 }
 
+let getBodyHTMLEmail = (dataSend) => {
+  let result = '';
+  if(dataSend.language === 'vi'){
+    result =
+     `
+    <h3>Xin chào ${dataSend.patientName}</h3>
+    <p>Bận nhận được Email này vì đã đặt lịch khám bệnh tại Booking Care thành công</p> 
+    <p>Thông tin đặt lịch khám bệnh:</p>
+     <div><b>Thời gian: ${dataSend.time}</b></div>
+     <div><b>Bác sĩ: ${dataSend.doctorName}</b></div>
+
+     <p>Nếu các thông tin trên là đúng, vui lòng nhấn vào đường link bên dưới
+     để xác nhận và hoàn tất thủ tục đặt lịch khám bệnh.
+     </p>
+     <div>
+         <a href=${dataSend.redirectLink} target="_blank">Click here</a>   
+         <div>Xin chân thành cảm ơn!</div>     
+     </div>
+    `
+}
+if(dataSend.language === 'en'){
+  result =
+  `
+  <h3>Dear ${dataSend.patientName}</h3>
+  <p>Busy received this email because you have successfully booked a medical appointment at Booking Care</p> 
+  <p>Information to schedule an appointment:</p>
+   <div><b>Time: ${dataSend.time}</b></div>
+   <div><b>Doctor: ${dataSend.doctorName}</b></div>
+
+   <p>If the above information is correct, please click the link below 
+   to confirm and complete the procedure to book an appointment.
+   </p>
+   <div>
+       <a href=${dataSend.redirectLink} target="_blank">Click here</a>   
+       <div>Sincerely thank!</div>     
+   </div>
+  `
+}
+  return result
+}
 
 module.exports = {
     sendSimpleEmail: sendSimpleEmail,
--- src/services/patientServices.js
+++ src/services/patientServices.js
@@ -5,17 +5,21 @@ import emailService from './emailService'
 let postBookAppointment = (data) => {
     return new Promise( async (resolve, reject) => {
         try{
-            if(!data.email || !data.doctorId || !data.timeType || !data.date){
+            if(!data.email || !data.doctorId || !data.timeType || !data.date
+                || !data.fullName
+                ){ 
                 resolve({
                     errCode: 1,
                     errMessage: 'Missing parameter'
                 })
             }else{
+               
               await emailService.sendSimpleEmail({
                 receiverEmail: data.email,
-                patientName: 'Duy Manh',
-                time: '10:00-11:00 CN 1/8/2023',
-                doctorName: 'Văn Trần',
+                patientName: data.fullName,
+                time: data.timeString,
+                doctorName: data.doctorName,
+                language: data.language,
                 redirectLink: 'https://www.youtube.com/watch?v=0GL--Adfqhc&list=PLncHg6Kn2JT6E38Z3kit9Hnif1xC_9VqI&index=97'
             })
 
